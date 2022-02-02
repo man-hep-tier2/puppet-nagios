675116fd468b35fa829f2c79c53295293372a828
rwf14f <robert.frank@manchester.ac.uk>
2022-02-02 14:20:31 +0000

    Add RockyLinux.

--- manifests/params.pp
+++ manifests/params.pp
@@ -46,7 +46,7 @@ class nagios::params(
   ]
 
   case $::operatingsystem {
-    'RedHat', 'Fedora', 'CentOS', 'Scientific', 'Amazon': {
+    'RedHat', 'Fedora', 'CentOS', 'Scientific', 'Amazon', 'Rocky': {
       $nrpe_package       = [ 'nrpe', 'nagios-plugins' ]
       $nrpe_package_alias = undef
       $nrpe_service       = 'nrpe'
